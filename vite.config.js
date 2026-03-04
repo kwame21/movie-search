0621f119d342b95bfa524b30f4d295eb303cfbb1
kwame21 <kwamemhone0@gmail.com>
2026-03-04 15:36:39 +0200

    add env variables to vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'import.meta.env.VITE_OMDB_API_KEY': JSON.stringify('4c120848'),
+    'import.meta.env.VITE_YOUTUBE_API_KEY': JSON.stringify('AIzaSyCJMkskxwdak2Gzpk6oQw6k6lBKFQDi4-4'),
+  }
 })
